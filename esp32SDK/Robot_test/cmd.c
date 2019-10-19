d646ff47efd4c76555134e41c845643c63acd014
liuyuelin <liuyuelin@autohejia.com>
2019-10-19 17:05:26 +0800

    sd

--- esp32SDK/Robot_test/cmd.c
+++ esp32SDK/Robot_test/cmd.c
@@ -4,191 +4,283 @@
 #include <string.h>
 #include <stdlib.h>
 #include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #include "freertos/queue.h"
+#include "freertos/task.h"
+#include "soc/timer_group_struct.h"
+#include "driver/periph_ctrl.h"
+#include "driver/timer.h"
 #include "driver/gpio.h"
+#include "esp_types.h"
+
+
 
 #define GPIO_LF_DIR    13
 #define GPIO_RF_DIR    27
-#define GPIO_LR_DIR    5
+#define GPIO_LR_DIR    18//5
 #define GPIO_RR_DIR    21
-#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_LF_DIR) | (1ULL<<GPIO_RF_DIR) | (1ULL<<GPIO_LR_DIR) | (1ULL<<GPIO_RR_DIR))
+#define GPIO_DIR_SEL  ((1ULL<<GPIO_LF_DIR) | (1ULL<<GPIO_RF_DIR) | (1ULL<<GPIO_LR_DIR) | (1ULL<<GPIO_RR_DIR))
 
+#define GPIO_LF_COL     2//12
+#define GPIO_RF_COL     4//26
+#define GPIO_LR_COL     5//18
+#define GPIO_RR_COL     22
+#define GPIO_PWM_SEL  ((1ULL<<GPIO_LF_COL) | (1ULL<<GPIO_RF_COL) | (1ULL<<GPIO_LR_COL) | (1ULL<<GPIO_RR_COL))
+#define GPIO_SEL (GPIO_DIR_SEL | GPIO_PWM_SEL)
 
-#define GPIO_FR_COL     34
-#define GPIO_LR_COL     35
-#define GPIO_UD_COL     36
-#define GPIO_RT_COL     39
-#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_FR_COL) | (1ULL<<GPIO_LR_COL) | (1ULL<<GPIO_UD_COL) | (1ULL<<GPIO_RT_COL))
-#define ESP_INTR_FLAG_DEFAULT 0
+#define TIMER_DIVIDER         1  //  Hardware timer clock divider
+#define TIMER_SCALE           (TIMER_BASE_CLK / TIMER_DIVIDER)  // convert counter value to seconds
+#define TIMER_INTERVAL0_SEC   (0.000005) // sample test interval for the first timer
+#define TIMER_INTERVAL1_SEC   (5.78)   // sample test interval for the second timer
+#define TEST_WITHOUT_RELOAD   0        // testing will be done without auto reload
+#define TEST_WITH_RELOAD      1        // testing will be done with auto reload
+typedef struct {
+    int type;  // the type of timer's event
+    int timer_group;
+    int timer_idx;
+    uint64_t timer_counter_value;
+} timer_event_t;
 
+CAR_INFO TheCar={
+    .LFront = {
+        .Value=0,//当前电平
+        .Dir=0,//方向
+        .ClkCnt=0x190, //速度
+        .Group=0,
+        .Timer=0,
+    },
+    .RFront = {
+        .Value=0,//当前电平
+        .Dir=0,//方向
+        .ClkCnt=0x190, //速度
+        .Group=0,
+        .Timer=1,
+    },
+    .LRear = {
+        .Value=0,//当前电平
+        .Dir=0,//方向
+        .ClkCnt=0x190, //速度
+        .Group=1,
+        .Timer=0,
+    },
+    .RRear = {
+        .Value=0,//当前电平
+        .Dir=0,//方向
+        .ClkCnt=0x190, //速度
+        .Group=1,
+        .Timer=1,
+    },
+};
+CONTROL_INFO TheControl ={
+    .BAfter = 100,
+    .LRight = 0,
+    .UDown  = 0,
+    .Rotating = 0,
+    };
 
-CAR_INFO TheCar;
-CONTROL_INFO TheControl;
-
-//卡尔曼滤波器
-uint32_t KalmanFiter(uint32_t Data,uint8_t index){
-    double TestPool = 5; 
-    double PredictPool =1;
-    double NowPool = 0;
-    double Credibility;
-    double Optimal;
-    static double LastOptimal[4] = {1,1,1,1};
-    static double LastOptimalPool[4] = {1,1,1,1};
-    NowPool = PredictPool + LastOptimalPool[index];
-    Credibility = sqrt(NowPool/(NowPool + TestPool));
-    Optimal = LastOptimal[index] + Credibility*(Data - LastOptimal[index]);
-    OptimalPool = NowPool*(1-Credibility);
-    LastOptimal[index] = Optimal;
-    LastOptimalPool[index] = OptimalPool;
-    return (uint32_t)Optimal;
-}
-
-//计算速度值及方向，并置位方向
-void SpeedIntegration(){
-    double A = 0.15;//左右轮间距/2
-    double B = 0.20;//前后轮间距/2
-    uint32_t StepPorM = 1000;//步数/S
-    global PwmInfo;
-    TheCar.LFront.ClkCnt = (TheControl.BAfter.Speed - TheControl.LRight.Speed + TheControl.Rotating.Speed*(A+B))*StepPorM;
-    TheCar.RFront.ClkCnt = (TheControl.BAfter.Speed + TheControl.LRight.Speed - TheControl.Rotating.Speed*(A+B))*StepPorM;
-    TheCar.LRear.ClkCnt  = (TheControl.BAfter.Speed - TheControl.LRight.Speed - TheControl.Rotating.Speed*(A+B))*StepPorM;
-    TheCar.RRear.ClkCnt  = (TheControl.BAfter.Speed + TheControl.LRight.Speed + TheControl.Rotating.Speed*(A+B))*StepPorM;
-    if(0 < TheCar.LFront.ClkCnt){
-        TheCar.LFront.ClkCnt = -1*TheCar.LFront.ClkCnt;
+void SpeedIntegration(void){
+    int32_t ClkCnt[4] = {0};
+    ClkCnt[0]  = TheControl.BAfter - TheControl.LRight + TheControl.Rotating;
+    ClkCnt[1]  = TheControl.BAfter + TheControl.LRight - TheControl.Rotating;
+    ClkCnt[2]  = TheControl.BAfter - TheControl.LRight - TheControl.Rotating;
+    ClkCnt[3]  = TheControl.BAfter + TheControl.LRight + TheControl.Rotating;
+    if(0 == ClkCnt[0]){
+        TheCar.LFront.ClkCnt = TIMER_SCALE;
+     }
+    else if(0 > ClkCnt[0]){
+        TheCar.LFront.ClkCnt = TIMER_SCALE/(-1*ClkCnt[0]);
         TheCar.LFront.Dir = 1;
     }
     else{
+        TheCar.LFront.ClkCnt = TIMER_SCALE/ClkCnt[0];
         TheCar.LFront.Dir = 0;
     }
-    if(0 < TheCar.RFront.ClkCnt){
-        TheCar.RFront.ClkCnt = -1*TheCar.RFront.ClkCnt;
+    
+    if(0 == ClkCnt[1]){
+        TheCar.LFront.ClkCnt = TIMER_SCALE;
+     }
+    else if(0 > ClkCnt[1]){
+        TheCar.RFront.ClkCnt = TIMER_SCALE/(-1*ClkCnt[1]);
         TheCar.RFront.Dir = 1;
     }
     else{
+        TheCar.RFront.ClkCnt = TIMER_SCALE/ClkCnt[1];
         TheCar.RFront.Dir = 0;
     }
-    if(0 < TheCar.LRear.ClkCnt){
-        TheCar.LRear.ClkCnt = -1*TheCar.LRear.ClkCnt;
+    
+    if(0 == ClkCnt[2]){
+        TheCar.LFront.ClkCnt = TIMER_SCALE;
+     }
+    else if(0 > ClkCnt[2]){
+        TheCar.LRear.ClkCnt = TIMER_SCALE/(-1*ClkCnt[2]);
         TheCar.LRear.Dir = 1;
     }
     else{
+        TheCar.LRear.ClkCnt = TIMER_SCALE/ClkCnt[2];
         TheCar.LRear.Dir = 0;
     }
-    if(0 < TheCar.RRear.ClkCnt){
-        TheCar.RRear.ClkCnt = -1*TheCar.RRear.ClkCnt;
+    
+    if(0 == ClkCnt[3]){
+        TheCar.LFront.ClkCnt = TIMER_SCALE;
+     }
+    else if(0 > ClkCnt[3]){
+        TheCar.RRear.ClkCnt = TIMER_SCALE/(-1*ClkCnt[3]);
         TheCar.RRear.Dir = 1;
     }
     else{
+        TheCar.RRear.ClkCnt = TIMER_SCALE/ClkCnt[3];
         TheCar.RRear.Dir = 0;
     }
     gpio_set_level(GPIO_LF_DIR, TheCar.LFront.Dir);
     gpio_set_level(GPIO_RF_DIR, TheCar.RFront.Dir);
-    gpio_set_level(GPIO_LR_DIR, TheCar.LRront.Dir);
-    gpio_set_level(GPIO_RR_DIR, TheCar.RRront.Dir);
+    gpio_set_level(GPIO_LR_DIR, TheCar.LRear.Dir);
+    gpio_set_level(GPIO_RR_DIR, TheCar.RRear.Dir);
 }
 
 void GPIO_init(void){
     gpio_config_t io_conf;
     io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
     io_conf.mode = GPIO_MODE_OUTPUT;
-    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    io_conf.pin_bit_mask = GPIO_SEL;
     io_conf.pull_up_en = 1;
     gpio_config(&io_conf);
-
-    io_conf.intr_type = GPIO_INTR_ANYEDGE;//边沿触发
-    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
-    io_conf.mode = GPIO_MODE_INPUT;
-    io_conf.pull_up_en = 1;
-    gpio_config(&io_conf);
-
-    //install gpio isr service
-    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
-    //hook isr handler for specific gpio pin
-    gpio_isr_handler_add(GPIO_FR_COL, FR_isr_handler, (void*) GPIO_FR_COL);
-    gpio_isr_handler_add(GPIO_LR_COL, LR_isr_handler, (void*) GPIO_LR_COL);
-    gpio_isr_handler_add(GPIO_UD_COL, UD_isr_handler, (void*) GPIO_UD_COL);
-    gpio_isr_handler_add(GPIO_RT_COL, RT_isr_handler, (void*) GPIO_RT_COL);
 }
-
-static void IRAM_ATTR FR_isr_handler(void* arg)
+/*
+ * Timer group0 ISR handler
+ */
+void IRAM_ATTR timer_group0_isr(void *para)
 {
-    static uint64_t start=0
-    if(rtc_gpio_get_level(GPIO_FR_COL)){
-        //获取系统时间
+    int timer_idx = (int) para;
+
+    /* Retrieve the interrupt status and the counter value
+       from the timer that reported the interrupt */
+    timer_intr_t timer_intr = timer_group_intr_get_in_isr(TIMER_GROUP_0);
+    uint64_t timer_counter_value = timer_group_get_counter_value_in_isr(TIMER_GROUP_0, timer_idx);
+
+    /* Prepare basic event data
+       that will be then sent back to the main program task */
+    timer_event_t evt;
+    evt.timer_group = 0;
+    evt.timer_idx = timer_idx;
+    evt.timer_counter_value = timer_counter_value;
+
+    /* Clear the interrupt
+       and update the alarm time for the timer with without reload */
+    if (timer_intr & TIMER_INTR_T0) {
+        evt.type = TEST_WITHOUT_RELOAD;
+        timer_group_intr_clr_in_isr(TIMER_GROUP_0, TIMER_0);
+        timer_counter_value += (uint64_t) (TIMER_INTERVAL0_SEC * TIMER_SCALE);
+        timer_group_set_alarm_value_in_isr(TIMER_GROUP_0, timer_idx, timer_counter_value);
+    } else if (timer_intr & TIMER_INTR_T1) {
+        evt.type = TEST_WITH_RELOAD;
+        timer_group_intr_clr_in_isr(TIMER_GROUP_0, TIMER_1);
+    } else {
+        evt.type = -1; // not supported even type
     }
-    else{
-        TheControl.BAfter.CTime=//获取时间
-    }
+    timer_group_enable_alarm_in_isr(TIMER_GROUP_0, timer_idx);
 }
-static void IRAM_ATTR LR_isr_handler(void* arg)
+
+
+/*
+ * Timer group0 ISR handler
+ */
+void IRAM_ATTR timer_group1_isr(void *para)
 {
-    static uint64_t start=0
-    if(rtc_gpio_get_level(GPIO_FR_COL)){
-        //获取系统时间
+    int timer_idx = (int) para;
+    TIMERG1.hw_timer[timer_idx].update = 1;
+    TIMERG1.int_clr_timers.t0 = 1;
+    uint64_t timer_counter_value = 
+        ((uint64_t) TIMERG1.hw_timer[timer_idx].cnt_high) << 32
+        | TIMERG1.hw_timer[timer_idx].cnt_low;
+    if(TheCar.RRear.Timer == timer_idx){
+        timer_counter_value += (uint64_t)TheCar.RRear.ClkCnt;
+        if(TheCar.RRear.Value){
+            TheCar.RRear.Value = 0;
+        }
+        else{
+            TheCar.RRear.Value = 1;
+        }
+        gpio_set_level(GPIO_RR_COL, TheCar.RRear.Value);
+
     }
     else{
-        TheControl.LRight.CTime=//获取时间
+        timer_counter_value += (uint64_t)TheCar.LRear.ClkCnt;
+        if(TheCar.LRear.Value){
+            TheCar.LRear.Value = 0;
+        }
+        else{
+            TheCar.LRear.Value = 1;
+        }
+        gpio_set_level(GPIO_LR_COL, TheCar.LRear.Value);
     }
+    TIMERG1.hw_timer[timer_idx].alarm_high = (uint32_t) (timer_counter_value >> 32);
+    TIMERG1.hw_timer[timer_idx].alarm_low = (uint32_t) timer_counter_value;
+    TIMERG1.hw_timer[timer_idx].config.alarm_en = TIMER_ALARM_EN;
 }
-static void IRAM_ATTR UD_isr_handler(void* arg)
+
+/*
+ * Initialize selected timer of the timer group 0
+ */
+void example_tg0_timer_init(int timer_idx,
+    bool auto_reload, double timer_interval_sec)
 {
-    static uint64_t start=0
-    if(rtc_gpio_get_level(GPIO_FR_COL)){
-        //获取系统时间
-    }
-    else{
-        TheControl.UDown.CTime=//获取时间
-    }
+    /* Select and initialize basic parameters of the timer */
+    timer_config_t config;
+    config.divider = TIMER_DIVIDER;
+    config.counter_dir = TIMER_COUNT_UP;
+    config.counter_en = TIMER_PAUSE;
+    config.alarm_en = TIMER_ALARM_EN;
+    config.intr_type = TIMER_INTR_LEVEL;
+    config.auto_reload = auto_reload;
+    timer_init(TIMER_GROUP_0, timer_idx, &config);
+
+    /* Timer's counter will initially start from value below.
+       Also, if auto_reload is set, this value will be automatically reload on alarm */
+    timer_set_counter_value(TIMER_GROUP_0, timer_idx, 0x00000000ULL);
+
+    /* Configure the alarm value and the interrupt on alarm. */
+    timer_set_alarm_value(TIMER_GROUP_0, timer_idx, timer_interval_sec * TIMER_SCALE);
+    timer_enable_intr(TIMER_GROUP_0, timer_idx);
+    timer_isr_register(TIMER_GROUP_0, timer_idx, timer_group0_isr,
+        (void *) timer_idx, ESP_INTR_FLAG_IRAM, NULL);
+
+    timer_start(TIMER_GROUP_0, timer_idx);
 }
-static void IRAM_ATTR RT_isr_handler(void* arg)
+
+
+void inline print_timer_counter(uint64_t counter_value)
 {
-    static uint64_t start=0
-    if(rtc_gpio_get_level(GPIO_FR_COL)){
-        //获取系统时间
-    }
-    else{
-        TheControl.Rotating.CTime=//获取时间
+    printf("Counter: 0x%08x%08x\n", (uint32_t) (counter_value >> 32),
+                                    (uint32_t) (counter_value));
+}
+
+/*
+ * The main task of this example program
+ */
+void timer_example_evt_task(void *arg)
+{
+    while (1) {
+        SpeedIntegration();
+    	vTaskDelay(pdMS_TO_TICKS(1000));
+        print_timer_counter(TheCar.LFront.ClkCnt);
+        print_timer_counter(TheCar.RFront.ClkCnt);
+        print_timer_counter(TheCar.LRear.ClkCnt);
+        print_timer_counter(TheCar.RRear.ClkCnt);
     }
 }
 
-//还缺一个遥控信号转速度的。
-void GetDutyTm(void){//获取占空比
-    uint32_t SignalMin = 1000;//#信号最小值
-    uint32_t SignalMiddle = 1500;//#信号中值
-    uint32_t SignalMax = 2000;//#信号最大值
-    uint32_t Precision = 500;//#理解为范围（SignalMiddle-SignalMin）
-    uint32_t SpeedMax = 2;//#最大速度m/s
-    double AngleSpeedMax = 6.28;//#最大角速度1转 弧度值 2*Pi
-    uint32_t AngleMax = 40;//#最大角度
-    uint32_t Signal = 0;
-    
-#前后
-    Signal=TheControl.BAfter.CTime;
-    if(Signal >SignalMiddle){
-        TheControl.BAfter.Speed = (Signal - SignalMiddle)/Precision * SpeedMax;
-    }
-    else{
-        TheControl.BAfter.Speed = -((SignalMiddle - Signal)/Precision * SpeedMax);
-    }
-#左右
-    Signal=TheControl.LRight.CTime;
-    if(Signal >SignalMiddle){
-        TheControl.LRight.Speed = (Signal - SignalMiddle)/Precision * SpeedMax;
-    }
-    else{
-        TheControl.LRight.Speed = -((SignalMiddle - Signal)/Precision * SpeedMax);
-    }
-#上下
-    Signal=TheControl.UDown.CTime;
-    TheControl.UDown.Speed = (Signal - SignalMin)/(Precision*2) * AngleMax;
-
-#自转
-    Signal=TheControl.Rotating.CTime;
-    if(Signal >SignalMiddle){
-        TheControl.Rotating.Speed = (Signal - SignalMiddle)/Precision * AngleSpeedMax;
-    }
-    else{
-        TheControl.Rotating.Speed = -((SignalMiddle - Signal)/Precision * AngleSpeedMax);
-    }
+void SystemInit(void){
+    print_timer_counter((uint64_t) (TIMER_INTERVAL0_SEC * TIMER_SCALE));
+    print_timer_counter(TheCar.LFront.ClkCnt);
+     printf("GPIO_init\n");
+    GPIO_init();
+    printf("wheel_timer_init0\n");
+    example_tg0_timer_init(TIMER_0, TEST_WITHOUT_RELOAD, TIMER_INTERVAL0_SEC);
+    //wheel_timer_init(TheCar.RFront.Group,TheCar.RFront.Timer, TEST_WITHOUT_RELOAD, TIMER_INTERVAL0_SEC);
+    printf("wheel_timer_init1\n");
+    //wheel_timer_init(TheCar.LFront.Group,TheCar.LFront.Timer, TEST_WITHOUT_RELOAD, TIMER_INTERVAL0_SEC);
+    printf("wheel_timer_init2\n");
+    //wheel_timer_init(TheCar.LRear.Group,TheCar.LRear.Timer, TEST_WITHOUT_RELOAD, TIMER_INTERVAL0_SEC);
+    printf("wheel_timer_init3\n");
+    //wheel_timer_init(TheCar.RRear.Group,TheCar.RRear.Timer, TEST_WITHOUT_RELOAD, TIMER_INTERVAL0_SEC);
 }
+
--- esp32SDK/Robot_test/cmd.h
+++ esp32SDK/Robot_test/cmd.h
@@ -1,9 +1,13 @@
-
+#ifndef __CMD_H__
+#define __CMD_H__
+#include "esp_types.h"
 
 typedef struct {
     uint8_t Value;//当前电平
     uint8_t Dir;//方向
-    double ClkCnt; //速度
+    uint64_t ClkCnt; //速度
+    int32_t Group;
+    int32_t Timer;
 } WHEEL_INFO;
 
 typedef struct {
@@ -13,16 +17,19 @@ typedef struct {
     WHEEL_INFO RRear;//右后
 } CAR_INFO;
 
-typedef struct {
-    uint32_t CTime;//时间
-    uint32_t Speed;//速度
-} CHANNEL_INFO;
-
-typedef struct {
-    CHANNEL_INFO BAfter;//前后油门
-    CHANNEL_INFO LRight; //左右油门
-    CHANNEL_INFO UDown;//云台上下
-    CHANNEL_INFO Rotating; //自转
+typedef struct{
+    int32_t BAfter;//前后油门
+    int32_t LRight; //左右油门
+    int32_t UDown;//云台上下
+    int32_t Rotating; //自转
 } CONTROL_INFO;
 
+extern CAR_INFO TheCar;
+extern CONTROL_INFO TheControl;
 
+extern void SpeedIntegration(void);
+extern void GetDutyTm(void);
+extern void timer_example_evt_task(void *arg);
+extern void SystemInit(void);
+
+#endif
