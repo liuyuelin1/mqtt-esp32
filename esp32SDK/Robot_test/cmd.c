e22aa32d97211c7a587dfb3b6d34f74d5abff4e7
liuyuelin <dhty2015@outlook.com>
2019-10-20 15:22:24 +0800

    已经可以输出四路PWM

--- esp32SDK/Robot_test/cmd.c
+++ esp32SDK/Robot_test/cmd.c
@@ -20,19 +20,20 @@
 #define GPIO_RR_DIR    21
 #define GPIO_DIR_SEL  ((1ULL<<GPIO_LF_DIR) | (1ULL<<GPIO_RF_DIR) | (1ULL<<GPIO_LR_DIR) | (1ULL<<GPIO_RR_DIR))
 
-#define GPIO_LF_COL     2//12
-#define GPIO_RF_COL     4//26
-#define GPIO_LR_COL     5//18
-#define GPIO_RR_COL     22
-#define GPIO_PWM_SEL  ((1ULL<<GPIO_LF_COL) | (1ULL<<GPIO_RF_COL) | (1ULL<<GPIO_LR_COL) | (1ULL<<GPIO_RR_COL))
+#define GPIO_LF_PWM     2//12
+#define GPIO_RF_PWM     4//26
+#define GPIO_LR_PWM     5//18
+#define GPIO_RR_PWM     22
+#define GPIO_PWM_SEL  ((1ULL<<GPIO_LF_PWM) | (1ULL<<GPIO_RF_PWM) | (1ULL<<GPIO_LR_PWM) | (1ULL<<GPIO_RR_PWM))
 #define GPIO_SEL (GPIO_DIR_SEL | GPIO_PWM_SEL)
 
-#define TIMER_DIVIDER         1  //  Hardware timer clock divider
+#define TIMER_DIVIDER         16  //  Hardware timer clock divider
 #define TIMER_SCALE           (TIMER_BASE_CLK / TIMER_DIVIDER)  // convert counter value to seconds
-#define TIMER_INTERVAL0_SEC   (0.000005) // sample test interval for the first timer
+#define TIMER_INTERVAL0_SEC   (3.4179) // sample test interval for the first timer
 #define TIMER_INTERVAL1_SEC   (5.78)   // sample test interval for the second timer
 #define TEST_WITHOUT_RELOAD   0        // testing will be done without auto reload
 #define TEST_WITH_RELOAD      1        // testing will be done with auto reload
+
 typedef struct {
     int type;  // the type of timer's event
     int timer_group;
@@ -71,12 +72,13 @@ CAR_INFO TheCar={
     },
 };
 CONTROL_INFO TheControl ={
-    .BAfter = 100,
+    .BAfter = 10,
     .LRight = 0,
     .UDown  = 0,
     .Rotating = 0,
     };
 
+uint64_t Cnt[4]={0};
 void SpeedIntegration(void){
     int32_t ClkCnt[4] = {0};
     ClkCnt[0]  = TheControl.BAfter - TheControl.LRight + TheControl.Rotating;
@@ -149,103 +151,138 @@ void GPIO_init(void){
  */
 void IRAM_ATTR timer_group0_isr(void *para)
 {
-    int timer_idx = (int) para;
+	int timer_idx = (int) para;
 
-    /* Retrieve the interrupt status and the counter value
-       from the timer that reported the interrupt */
-    timer_intr_t timer_intr = timer_group_intr_get_in_isr(TIMER_GROUP_0);
-    uint64_t timer_counter_value = timer_group_get_counter_value_in_isr(TIMER_GROUP_0, timer_idx);
+	/* Retrieve the interrupt status and the counter value
+	   from the timer that reported the interrupt */
+	uint32_t intr_status = TIMERG0.int_st_timers.val;
+	TIMERG0.hw_timer[timer_idx].update = 1;
+	uint64_t timer_counter_value = 
+		((uint64_t) TIMERG0.hw_timer[timer_idx].cnt_high) << 32
+		| TIMERG0.hw_timer[timer_idx].cnt_low;
 
-    /* Prepare basic event data
-       that will be then sent back to the main program task */
-    timer_event_t evt;
-    evt.timer_group = 0;
-    evt.timer_idx = timer_idx;
-    evt.timer_counter_value = timer_counter_value;
+	/* Clear the interrupt
+	   and update the alarm time for the timer with without reload */
+	if (timer_idx == TIMER_0) {
+		Cnt[0]++;
+		TIMERG0.int_clr_timers.t0 = 1;
+		timer_counter_value += TheCar.LFront.ClkCnt;
+		if(TheCar.LFront.Value){
+			gpio_set_level(GPIO_LF_PWM, 1);
+			TheCar.LFront.Value = 0;
+		}
+		else{
+			gpio_set_level(GPIO_LF_PWM, 0);
+			TheCar.LFront.Value = 1;
+		}
 
-    /* Clear the interrupt
-       and update the alarm time for the timer with without reload */
-    if (timer_intr & TIMER_INTR_T0) {
-        evt.type = TEST_WITHOUT_RELOAD;
-        timer_group_intr_clr_in_isr(TIMER_GROUP_0, TIMER_0);
-        timer_counter_value += (uint64_t) (TIMER_INTERVAL0_SEC * TIMER_SCALE);
-        timer_group_set_alarm_value_in_isr(TIMER_GROUP_0, timer_idx, timer_counter_value);
-    } else if (timer_intr & TIMER_INTR_T1) {
-        evt.type = TEST_WITH_RELOAD;
-        timer_group_intr_clr_in_isr(TIMER_GROUP_0, TIMER_1);
-    } else {
-        evt.type = -1; // not supported even type
-    }
-    timer_group_enable_alarm_in_isr(TIMER_GROUP_0, timer_idx);
+	} else if (timer_idx == TIMER_1) {
+		Cnt[1]++;
+		TIMERG0.int_clr_timers.t1 = 1;
+		timer_counter_value += TheCar.RFront.ClkCnt;
+		if(TheCar.RFront.Value){
+			gpio_set_level(GPIO_RF_PWM, 1);
+			TheCar.RFront.Value = 0;
+		}
+		else{
+			gpio_set_level(GPIO_RF_PWM, 0);
+			TheCar.RFront.Value = 1;
+		}
+
+	} else {
+		timer_counter_value += (uint64_t) (1 * TIMER_SCALE);
+	}
+	TIMERG0.hw_timer[timer_idx].alarm_high = (uint32_t) (timer_counter_value >> 32);
+	TIMERG0.hw_timer[timer_idx].alarm_low = (uint32_t) timer_counter_value;
+
+	/* After the alarm has been triggered
+	  we need enable it again, so it is triggered the next time */
+	TIMERG0.hw_timer[timer_idx].config.alarm_en = TIMER_ALARM_EN;
+
+	/* Now just send the event data back to the main program task */
 }
 
 
 /*
- * Timer group0 ISR handler
+ * Timer group1 ISR handler
  */
 void IRAM_ATTR timer_group1_isr(void *para)
 {
-    int timer_idx = (int) para;
-    TIMERG1.hw_timer[timer_idx].update = 1;
-    TIMERG1.int_clr_timers.t0 = 1;
-    uint64_t timer_counter_value = 
-        ((uint64_t) TIMERG1.hw_timer[timer_idx].cnt_high) << 32
-        | TIMERG1.hw_timer[timer_idx].cnt_low;
-    if(TheCar.RRear.Timer == timer_idx){
-        timer_counter_value += (uint64_t)TheCar.RRear.ClkCnt;
-        if(TheCar.RRear.Value){
-            TheCar.RRear.Value = 0;
-        }
-        else{
-            TheCar.RRear.Value = 1;
-        }
-        gpio_set_level(GPIO_RR_COL, TheCar.RRear.Value);
+	int timer_idx = (int) para;
 
-    }
-    else{
-        timer_counter_value += (uint64_t)TheCar.LRear.ClkCnt;
-        if(TheCar.LRear.Value){
-            TheCar.LRear.Value = 0;
-        }
-        else{
-            TheCar.LRear.Value = 1;
-        }
-        gpio_set_level(GPIO_LR_COL, TheCar.LRear.Value);
-    }
-    TIMERG1.hw_timer[timer_idx].alarm_high = (uint32_t) (timer_counter_value >> 32);
-    TIMERG1.hw_timer[timer_idx].alarm_low = (uint32_t) timer_counter_value;
-    TIMERG1.hw_timer[timer_idx].config.alarm_en = TIMER_ALARM_EN;
+	/* Retrieve the interrupt status and the counter value
+	   from the timer that reported the interrupt */
+	uint32_t intr_status = TIMERG1.int_st_timers.val;
+	TIMERG1.hw_timer[timer_idx].update = 1;
+	uint64_t timer_counter_value = 
+		((uint64_t) TIMERG1.hw_timer[timer_idx].cnt_high) << 32
+		| TIMERG1.hw_timer[timer_idx].cnt_low;
+	if (timer_idx == TIMER_0) {
+		Cnt[2]++;
+		TIMERG1.int_clr_timers.t0 = 1;
+		timer_counter_value += TheCar.LRear.ClkCnt;
+		if(TheCar.LRear.Value){
+			gpio_set_level(GPIO_LR_PWM, 1);
+			TheCar.LRear.Value = 0;
+		}
+		else{
+			gpio_set_level(GPIO_LR_PWM, 0);
+			TheCar.LRear.Value = 1;
+		}
+
+	}
+	else if (timer_idx == TIMER_1) {
+		Cnt[3]++;
+		TIMERG1.int_clr_timers.t1 = 1;
+		timer_counter_value += TheCar.RRear.ClkCnt;
+		if(TheCar.RRear.Value){
+			gpio_set_level(GPIO_RR_PWM, 1);
+			TheCar.RRear.Value = 0;
+		}
+		else{
+			gpio_set_level(GPIO_RR_PWM, 0);
+			TheCar.RRear.Value = 1;
+		}
+	} else {
+		timer_counter_value += (uint64_t) (1 * TIMER_SCALE);
+	}
+	TIMERG1.hw_timer[timer_idx].alarm_high = (uint32_t) (timer_counter_value >> 32);
+	TIMERG1.hw_timer[timer_idx].alarm_low = (uint32_t) timer_counter_value;
+	/* After the alarm has been triggered
+	  we need enable it again, so it is triggered the next time */
+	TIMERG1.hw_timer[timer_idx].config.alarm_en = TIMER_ALARM_EN;
+
 }
 
 /*
  * Initialize selected timer of the timer group 0
  */
-void example_tg0_timer_init(int timer_idx,
-    bool auto_reload, double timer_interval_sec)
+void example_timer_init(int group_idx,int timer_idx, void (*fn)(void*),
+	bool auto_reload, double timer_interval_sec)
 {
-    /* Select and initialize basic parameters of the timer */
-    timer_config_t config;
-    config.divider = TIMER_DIVIDER;
-    config.counter_dir = TIMER_COUNT_UP;
-    config.counter_en = TIMER_PAUSE;
-    config.alarm_en = TIMER_ALARM_EN;
-    config.intr_type = TIMER_INTR_LEVEL;
-    config.auto_reload = auto_reload;
-    timer_init(TIMER_GROUP_0, timer_idx, &config);
+	/* Select and initialize basic parameters of the timer */
+	timer_config_t config;
+	config.divider = TIMER_DIVIDER;
+	config.counter_dir = TIMER_COUNT_UP;
+	config.counter_en = TIMER_PAUSE;
+	config.alarm_en = TIMER_ALARM_EN;
+	config.intr_type = TIMER_INTR_LEVEL;
+	config.auto_reload = auto_reload;
+	timer_init(group_idx, timer_idx, &config);
 
-    /* Timer's counter will initially start from value below.
-       Also, if auto_reload is set, this value will be automatically reload on alarm */
-    timer_set_counter_value(TIMER_GROUP_0, timer_idx, 0x00000000ULL);
+	/* Timer's counter will initially start from value below.
+	   Also, if auto_reload is set, this value will be automatically reload on alarm */
+	timer_set_counter_value(group_idx, timer_idx, 0x00000000ULL);
 
-    /* Configure the alarm value and the interrupt on alarm. */
-    timer_set_alarm_value(TIMER_GROUP_0, timer_idx, timer_interval_sec * TIMER_SCALE);
-    timer_enable_intr(TIMER_GROUP_0, timer_idx);
-    timer_isr_register(TIMER_GROUP_0, timer_idx, timer_group0_isr,
-        (void *) timer_idx, ESP_INTR_FLAG_IRAM, NULL);
-
-    timer_start(TIMER_GROUP_0, timer_idx);
+	/* Configure the alarm value and the interrupt on alarm. */
+	timer_set_alarm_value(group_idx, timer_idx, timer_interval_sec * TIMER_SCALE);
+	timer_enable_intr(group_idx, timer_idx);
+	timer_isr_register(group_idx, timer_idx, fn, 
+		(void *) timer_idx, ESP_INTR_FLAG_IRAM, NULL);
+	timer_start(group_idx, timer_idx);
 }
 
+//TIMER_GROUP_0
 
 void inline print_timer_counter(uint64_t counter_value)
 {
@@ -262,9 +299,14 @@ void timer_example_evt_task(void *arg)
         SpeedIntegration();
     	vTaskDelay(pdMS_TO_TICKS(1000));
         print_timer_counter(TheCar.LFront.ClkCnt);
-        print_timer_counter(TheCar.RFront.ClkCnt);
-        print_timer_counter(TheCar.LRear.ClkCnt);
-        print_timer_counter(TheCar.RRear.ClkCnt);
+        print_timer_counter(Cnt[0]);
+	    print_timer_counter(Cnt[1]);
+	    print_timer_counter(Cnt[2]);
+	    print_timer_counter(Cnt[3]);
+		Cnt[0]=0;
+		Cnt[1]=0;
+		Cnt[2]=0;
+		Cnt[3]=0;
     }
 }
 
@@ -274,7 +316,13 @@ void SystemInit(void){
      printf("GPIO_init\n");
     GPIO_init();
     printf("wheel_timer_init0\n");
-    example_tg0_timer_init(TIMER_0, TEST_WITHOUT_RELOAD, TIMER_INTERVAL0_SEC);
+    example_timer_init(TIMER_GROUP_0,TIMER_0, timer_group0_isr,TEST_WITHOUT_RELOAD, TIMER_INTERVAL0_SEC);
+    example_timer_init(TIMER_GROUP_0,TIMER_1, timer_group0_isr,TEST_WITHOUT_RELOAD, TIMER_INTERVAL0_SEC);
+    example_timer_init(TIMER_GROUP_1,TIMER_0, timer_group1_isr,TEST_WITHOUT_RELOAD, TIMER_INTERVAL0_SEC);
+    example_timer_init(TIMER_GROUP_1,TIMER_1, timer_group1_isr,TEST_WITHOUT_RELOAD, TIMER_INTERVAL0_SEC);
+//    example_tg0_timer_init(TIMER_0, TEST_WITHOUT_RELOAD, TIMER_INTERVAL0_SEC);
+//    example_tg0_timer_init(TIMER_0, TEST_WITHOUT_RELOAD, TIMER_INTERVAL0_SEC);
+
     //wheel_timer_init(TheCar.RFront.Group,TheCar.RFront.Timer, TEST_WITHOUT_RELOAD, TIMER_INTERVAL0_SEC);
     printf("wheel_timer_init1\n");
     //wheel_timer_init(TheCar.LFront.Group,TheCar.LFront.Timer, TEST_WITHOUT_RELOAD, TIMER_INTERVAL0_SEC);
